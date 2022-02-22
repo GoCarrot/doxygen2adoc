9a6d10b66b6da5b6f49167f38653f93d26190a44
Pat Wilson <zerostride@gmail.com>
2022-02-22 13:45:57 -0800

    Better deprecated note support and parsing
    Also use proper JS private syntax

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ self.build = (inputPath, sourcePath) => {
   const parser = new XMLParser({
     ignoreAttributes: false,
     attributeNamePrefix: '$',
-    stopNodes: [
-      'doxygen.compounddef.sectiondef.memberdef.type',
-    ],
+    stopNodes: ['*.type', '*.xrefdescription'],
     tagValueProcessor: (tagName, tagValue, jPath, hasAttributes, isLeafNode) => {
       if (tagName === 'type') {
+        // console.log(`TYPE: ${tagValue}`);
         const type = tagValueParser.parse(`<type>${tagValue}</type>`)[0].type;
         const combinedType = type.reduce((str, elem) => {
           // TODO: Here is where we turn the ref into a link to docs for that ref
@@ -35,6 +34,15 @@ self.build = (inputPath, sourcePath) => {
           return `${str} ${typeText}`;
         }, '');
         return combinedType;
+      } else if (tagName === 'xrefdescription') {
+        const xrefdescription = tagValueParser.parse(`<xrefdescription>${tagValue}</xrefdescription>`)[0].xrefdescription[0];
+        const combined = xrefdescription.para.reduce((str, elem) => {
+          elem = elem.computeroutput ? elem.computeroutput[0] : elem;
+          const text = elem.ref ? elem.ref[0].$text : elem.$text;
+
+          return `${str} ${text}`;
+        }, '');
+        return combined;
       }
       return undefined;
     },
