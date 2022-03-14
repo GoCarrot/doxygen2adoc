4ea56e7e53d180e779eb80782cfafbbb8f20ca34
Pat Wilson <zerostride@gmail.com>
2022-03-14 12:24:16 -0700

    Now support creating links to types within the package

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ import CompoundRef from './lib/CompoundRef.js';
 const self = {};
 
 self.build = (inputPath, sourcePath) => {
+  const refMapping = {};
+
   // Parse tag values which contain order-dependent XML
   const tagValueParser = new XMLParser({
     ignoreAttributes: false,
     attributeNamePrefix: '$',
     textNodeName: '$text',
     preserveOrder: true,
+  });
+
+  // Undecorated parser
+  const firstPass = new XMLParser({
+    ignoreAttributes: false,
+    attributeNamePrefix: '$',
   });
 
   // XML Parser for general use
@@ -32,25 +40,39 @@ self.build = (inputPath, sourcePath) => {
         }
 
         const type = tagValueParser.parse(`<type>${tagValue}</type>`)[0].type;
-        return type.reduce((str, elem) => {
+        const combinedType = type.reduce((str, elem) => {
           // Strip out Doxygen including spaces in template types
           let text = elem.ref ? elem.ref[0].$text : elem.$text;
-          text = text.replaceAll('< ', '<').replaceAll(' >', '>');
+          text = text.replaceAll('< ', '<')
+              .replaceAll(' >', '>')
+              .replaceAll('>', '&gt;')
+              .replaceAll('<', '&lt;');
 
           if (elem.ref) {
             assert.strictEqual(elem.ref.length, 1, 'More than one ref during type parsing');
-            return `${str}${text.endsWith('>') || str.endsWith('<') ? '' : ' '}${text}`.trim();
 
-            // TODO: This is only used inside 'type' tags, and the end result is
+            // This is only used inside 'type' tags, and the end result is
             // that it's only showing up in the 'declaration' on rendered pages.
             //
             // The 'declaration' is rendered in a source block, so putting adoc
-            // markup inside that block is not useful.
-            // return `${str} xref:${elem[':@'].$refid}.adoc[${text}]`
+            // markup inside that block is not useful. Instead we have to directly
+            // render it as HTML.
+
+            // TODO: This is hard-coded formatting, it would be better to have some
+            //       kind of partial-template, but right now the CLI is the only
+            //       thing that knows about anything template related.
+            const refId = elem[':@'].$refid;
+            const refSource = refMapping[refId];
+            const refText = refSource ?
+              `<a href="${refSource}.html#${refId}">${text}</a>` :
+              `<a href="${refId}.html">${text}</a>`;
+
+            return `${str}${text.endsWith('&gt;') || str.endsWith('&lt;') ? '' : ' '}${refText}`.trim();
           } else {
-            return `${str}${text.endsWith('>') || str.endsWith('<') ? '' : ' '}${text}`.trim();
+            return `${str}${text.endsWith('&gt;') || str.endsWith('&lt;') ? '' : ' '}${text}`.trim();
           }
         }, '');
+        return combinedType;
       }
       return undefined;
     },
@@ -71,7 +93,23 @@ self.build = (inputPath, sourcePath) => {
       console.log(`Reading file ${file}`); // TODO: Verbose
       return parser.parse(fs.readFileSync(file, 'utf8'));
     },
+
+    firstPassXml: (name) => {
+      const file = path.join(inputPath, `${name}.xml`);
+      console.log(`Reading file ${file}`); // TODO: Verbose
+      return firstPass.parse(fs.readFileSync(file, 'utf8'));
+    },
   };
+
+  // Do a first-pass parse and build reference mapping as needed
+  provider.firstPassXml('index').doxygenindex.compound.forEach((compound) => {
+    if (compound.member) {
+      const members = Array.isArray(compound.member) ? compound.member : [compound.member];
+      members.forEach((member) => {
+        refMapping[member.$refid] = compound.$refid;
+      });
+    }
+  });
 
   // Read the index, create compound refs and return them
   const index = provider.xml('index');
